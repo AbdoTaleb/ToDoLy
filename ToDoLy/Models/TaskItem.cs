862402302070025d351a20c3f0fa97ec300a912b
Abdo Taleb <44838067+AbdoTaleb@users.noreply.github.com>
2025-06-10 10:11:39 +0200

    Add project files.

---
+++ ToDoLy/Models/TaskItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoLy.Models
+{
+    public class TaskItem
+    {
+        public string Title { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsDone { get; set; }
+        public string Project { get; set; } 
+
+        public TaskItem(string title, DateTime dueDate, string project)
+        {
+            Title = title;
+            DueDate = dueDate;
+            IsDone = false;
+            Project = project;
+        }
+
+        public void MarkAsDone()
+        {
+            IsDone = true;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} | Due: {DueDate.ToShortDateString()} | Project: {Project} | Done: {IsDone}";
+        }
+    }
+}
